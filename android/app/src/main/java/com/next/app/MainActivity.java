be3f8b422096d0d03b964393e2e491de76cb95c0
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2023-06-01 15:42:47 +0200

    add capacitor and native folders

---
+++ android/app/src/main/java/com/next/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.next.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.next.app",
+  appName: "next-capacitor-poc",
+  webDir: "out",
+  // server: {
+  //   url: "http://192.168.1.79:3000",
+  //   cleartext: true,
+  // },
+};
+
+export default config;
